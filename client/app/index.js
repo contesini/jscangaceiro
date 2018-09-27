03d1d90f0658b7b7ddc659b709c6c294e8d889a0
desenvolvimento1 <estagio.ti@piscinafacil.com.br>
2018-09-27 20:08:45 -0300

    save pagina 30

---
+++ client/app/index.js
@@ -0,0 +1,7 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+// precisamos de tbody, pois ele receberá a tr que vamos construir
+var tbody = document.querySelector('table tbody');
